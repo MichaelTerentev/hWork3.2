011d902093f958f570088573821350542385e7f3
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-04-24 20:11:19 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+     SinglyLinkedList<String> list = new SinglyLinkedList<>();
+     //добавление элементов
+     list.add("Hello world");
+     list.add("123");
+     list.add("abcdef");
+     //удаление элемента
+     list.remove(1);
+     //вывод элементов списка в консоль
+     for(int i = 0; i < list.size(); i++) {
+         System.out.println(list.get(i));
+     }
 }
